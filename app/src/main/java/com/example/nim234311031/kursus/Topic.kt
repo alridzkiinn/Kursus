3b9aef3405b14e8a6dc267e027bab7cc512d4e0c
ASUSAL\alriz <alriskialma@gmail.com>
2024-12-10 19:36:53 +0700

    KursusBy234311031

---
+++ app/src/main/java/com/example/nim234311031/kursus/Topic.kt
@@ -0,0 +1,10 @@
+package com.example.nim234311031.kursus
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
